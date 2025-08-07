9364b4551c8efb9b01f179e367a8197d1c184f70
kasunvimarshana <kasunvmail@gmail.com>
2025-08-08 04:27:53 +0530

    refactor header

--- wp-content/themes/aqualuxe/header.php
+++ wp-content/themes/aqualuxe/header.php
@@ -23,35 +23,42 @@
   <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'aqualuxe'); ?></a>
   
   <header id="masthead" class="site-header">
-    <div class="site-branding">
-      <?php
-      the_custom_logo();
-      if (is_front_page() && is_home()) :
-        ?>
-        <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+    <div class="header-container">
+      <div class="site-branding">
         <?php
-      else :
+        the_custom_logo();
+        if (is_front_page() && is_home()) :
+          ?>
+          <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+          <?php
+        else :
+          ?>
+          <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+          <?php
+        endif;
+        
+        $aqualuxe_description = get_bloginfo('description', 'display');
+        if ($aqualuxe_description || is_customize_preview()) :
+          ?>
+          <p class="site-description"><?php echo $aqualuxe_description; ?></p>
+          <?php
+        endif;
         ?>
-        <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-        <?php
-      endif;
+      </div><!-- .site-branding -->
       
-      $aqualuxe_description = get_bloginfo('description', 'display');
-      if ($aqualuxe_description || is_customize_preview()) :
-        ?>
-        <p class="site-description"><?php echo $aqualuxe_description; ?></p>
+      <nav id="site-navigation" class="main-navigation">
+        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+          <span class="menu-icon"></span>
+          <span class="menu-text"><?php esc_html_e('Menu', 'aqualuxe'); ?></span>
+        </button>
         <?php
-      endif;
-      ?>
-    </div><!-- .site-branding -->
-    
-    <nav id="site-navigation" class="main-navigation">
-      <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'aqualuxe'); ?></button>
-      <?php
-      wp_nav_menu(array(
-        'theme_location' => 'primary',
-        'menu_id' => 'primary-menu',
-      ));
-      ?>
-    </nav><!-- #site-navigation -->
+        wp_nav_menu(array(
+          'theme_location' => 'primary',
+          'menu_id' => 'primary-menu',
+          'container' => 'div',
+          'container_class' => 'menu-container',
+        ));
+        ?>
+      </nav><!-- #site-navigation -->
+    </div><!-- .header-container -->
   </header><!-- #masthead -->
