dbe4be201a7cbae1d9ccd7217358bb1b25426511
Kasun <kasun.v@synaxiom.com>
2025-09-06 02:20:55 +0530

    init

--- wp-content/themes/aqualuxe/modules/auctions/templates/auction-badge.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template for displaying an auction badge on product loops.
- */
-?>
-<div class="aqualuxe-auction-badge">
-    <span class="badge-icon">Gavel</span>
-    <span class="badge-text"><?php \_e('Auction', 'aqualuxe'); ?></span>
-</div>
